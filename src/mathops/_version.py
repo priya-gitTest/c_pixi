acdd122e2113fec397dfc745ed6da8ddab679dea
Priyanka O <21082240+priya-gitTest@users.noreply.github.com>
2025-10-27 15:26:33 +0000

    version change

--- src/mathops/_version.py
+++ src/mathops/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.2.dev0+g158275efe.d20251027'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev0', 'g158275efe.d20251027')
+__version__ = version = '0.0.3.dev0+g4987da9d3.d20251027'
+__version_tuple__ = version_tuple = (0, 0, 3, 'dev0', 'g4987da9d3.d20251027')
 
 __commit_id__ = commit_id = None
